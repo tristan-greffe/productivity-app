3bd47d2acfd7a7e473423f7ee7eefe68c3376dac
tristan <tristan.greffe@hotmail.com>
2023-02-17 19:28:51 +0100

    chore: Add bootstrap js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './assets/main.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min';
 
 createApp(App).use(router).mount('#app');
