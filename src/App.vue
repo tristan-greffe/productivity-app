7eb2b67750bd549cbbf49771320531e1b3665248
tristan <tristan.greffe@hotmail.com>
2023-02-17 14:25:06 +0100

    feat: Add display a hundred user profiles with Faker

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/todolist">TodoList</router-link> |
     <router-link to="/signup">SignUp</router-link> |
-    <router-link to="/user">User</router-link> |
+    <router-link to="/users">User</router-link> |
     <router-link to="/film">Film</router-link> |
     <router-link to="/meteo">Meteo</router-link> |
     <router-link to="/velib">Velib</router-link>
---
+++ src/views/Users.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="user-page">
+    <h1 class="title">Users</h1>
+    <div class="container d-flex flex-wrap justify-content-center">
+      <div class="card m-2" style="width: 18rem;" v-for="user in usersList" :key="user.firstName">
+        <User :user="user"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { faker } from '@faker-js/faker';
+import User from '../components/User.vue';
+
+export default {
+  name: 'user-page',
+  components: {
+    User,
+  },
+  data() {
+    return {
+      usersList: [],
+    };
+  },
+  created() {
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < 100; i++) {
+      this.usersList.push(
+        {
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName(),
+          telephone: faker.phone.phoneNumber('+336 ## ## ## ##'),
+          email: faker.internet.email(),
+          job: faker.name.jobTitle(),
+          avatar: faker.image.avatar(),
+        },
+      );
+    }
+  },
+};
+</script>
